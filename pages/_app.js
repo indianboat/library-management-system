a5f03c2406538580677b3ca3cead4e62ed0f0f0e
unknown <iampankaj852@gmail.com>
2022-12-26 19:57:13 +0530

    adding tailwind + nextui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextUIProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </NextUIProvider>
+    </>
+  );
 }
